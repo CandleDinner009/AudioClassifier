47cc9c2b8a36612d5080478a185fa5c48d785f4b
CandleDinner009 <monis.khursheed.ug21@nsut.ac.in>
2024-04-02 19:27:52 +0530

    initial commit

---
+++ resample.py
@@ -0,0 +1,14 @@
+import os
+from pydub import AudioSegment as am
+
+directory = "data/Parsed_Capuchinbird_Clips"
+for filename in os.scandir(directory):
+    sound = am.from_file(filename, format='wav', frame_rate=44100)
+    sound = sound.set_frame_rate(16000)
+    sound.export(filename, format='wav')
+
+directory2 = "data/Parsed_Not_Capuchinbird_Clips"
+for filename in os.scandir(directory2):
+    sound = am.from_file(filename, format='wav', frame_rate=44100)
+    sound = sound.set_frame_rate(16000)
+    sound.export(filename, format='wav')
